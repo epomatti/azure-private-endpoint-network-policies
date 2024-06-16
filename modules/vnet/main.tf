3f9a61a01fbbd220ef2b6cdd51e7ac31474ea36f
Evandro Pomatti <evandro.pomatti@gmail.com>
2024-06-16 15:14:06 -0300

    feat: explicit subnet private endpoint configuration

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -8,15 +8,25 @@ resource "azurerm_virtual_network" "default" {
 
 ### Subnets ###
 resource "azurerm_subnet" "vms" {
-  name                 = "virtual-machines"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.default.name
-  address_prefixes     = ["10.0.20.0/24"]
+  name                            = "virtual-machines"
+  resource_group_name             = var.resource_group_name
+  virtual_network_name            = azurerm_virtual_network.default.name
+  address_prefixes                = ["10.0.20.0/24"]
+  default_outbound_access_enabled = true
+
+  # This is already the default; just showing it here for reference
+  private_link_service_network_policies_enabled = true
+  private_endpoint_network_policies             = "Enabled"
 }
 
 resource "azurerm_subnet" "pes" {
-  name                 = "private-endpoints"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.default.name
-  address_prefixes     = ["10.0.70.0/24"]
+  name                            = "private-endpoints"
+  resource_group_name             = var.resource_group_name
+  virtual_network_name            = azurerm_virtual_network.default.name
+  address_prefixes                = ["10.0.70.0/24"]
+  default_outbound_access_enabled = true
+
+  # This is already the default; just showing it here for reference
+  private_link_service_network_policies_enabled = true
+  private_endpoint_network_policies             = "Enabled"
 }
